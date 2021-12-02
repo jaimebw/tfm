1f45540d7026659e1994fe003827b41b351ec5ab
jaimebw <jaimebwv@gmail.com>
2021-12-02 13:52:41 +0100

    Mejoras para ejecutar las pipelines

--- scripts/pgf_converter.py
+++ scripts/pgf_converter.py
@@ -1,5 +1,4 @@
-# This scrips is for obtaining all the data set representation into
-# .pgf images
+# This scrips is for obtaining all the data set representation into .pgf images
 # Reference: https://timodenk.com/blog/exporting-matplotlib-plots-to-latex/
 # You might need to add \usepackage{pgfplots} to your main.tex file
 # To add the images you just need to:
@@ -9,7 +8,8 @@
     #     \end{center}
     #     \caption{Your caption}
     # \end{figure}
-
+import sys
+from tqdm import tqdm
 from pathlib import Path
 import pandas as pd
 import numpy as np
@@ -25,11 +25,45 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
+pgf_imgs = Path("pgf_img")
+if not pgf_imgs.exists():
+    pgf_imgs.mkdir()
 
 def rms(values):
     # return the Root Mean Square 
     # More info at https://en.wikipedia.org/wiki/Root_mean_square
     return np.sqrt(np.sum(values**2)/len(values))
+
+def plot_rolling_average_stats(df,dataset_name,period):
+    cols = df.columns.to_list()[:-1]
+    df = df.abs()
+    df1 = df.groupby(["timestamp"]).mean().reset_index()
+    df1.index = pd.to_datetime(df1.timestamp,unit='s')
+    df1 = df1.rolling(window = period).mean()
+    with plt.style.context('seaborn-paper'):
+        myFmt = mdates.DateFormatter('%d/%m')
+        fig,ax = plt.subplots()
+        if len(cols) <6:
+            ax.plot(df1.b1_ch1,label = "Bearing 1")
+            ax.plot(df1.b2_ch2,label = "Bearing 2")
+            ax.plot(df1.b3_ch3,label = "Bearing 3")
+            ax.plot(df1.b4_ch4,label = "Bearing 4")
+        else:
+            ax.plot(df1.b1_ch1,label = "Bearing 1 C-X")
+            ax.plot(df1.b1_ch2,label = "Bearing 1 C-Y ")
+            ax.plot(df1.b2_ch3,label = "Bearing 2 C-X")
+            ax.plot(df1.b2_ch4,label = "Bearing 2 C-Y")
+            
+            ax.plot(df1.b3_ch5,label = "Bearing 3 C-X")
+            ax.plot(df1.b3_ch6,label = "Bearing 3 C-Y")
+            ax.plot(df1.b4_ch7,label = "Bearing 4 C-X")
+            ax.plot(df1.b4_ch8,label = "Bearing 4 C-Y")
+        ax.set_xlabel ("Days")
+        ax.legend()
+        ax.xaxis.set_major_formatter(myFmt)
+        ax.grid()
+        ax.set_title(f"Rolling average(period = {period}) of the bearing vibration snapshots")
+        fig.savefig(dataset_name+".pgf")
 
 
 def plot_rms_general_stats(df,dataset_name):
@@ -37,7 +71,7 @@ def plot_rms_general_stats(df,dataset_name):
     df1 = df.groupby(["timestamp"])[cols].apply(rms).reset_index()
     df1.index = pd.to_datetime(df1.timestamp,unit='s')
     with plt.style.context('seaborn-paper'):
-        myFmt = mdates.DateFormatter('%d')
+        myFmt = mdates.DateFormatter('%d/%m')
         fig,ax = plt.subplots()
         if len(cols) <6:
             ax.plot(df1.b1_ch1,label = "Bearing 1")
@@ -62,13 +96,14 @@ def plot_rms_general_stats(df,dataset_name):
         fig.savefig(dataset_name+".pgf")
         #save_fig(fig,"{}.png".format(dataset_name),figsize = (12,6),dpi = 300)
 
+
 def plot_mean_abs_general_stats(df,dataset_name):
     cols = df.columns.to_list()[:-1]
     df = df.abs()
     df1 = df.groupby(["timestamp"]).mean().reset_index()
     df1.index = pd.to_datetime(df1.timestamp,unit='s')
     with plt.style.context('seaborn-paper'):
-        myFmt = mdates.DateFormatter('%d')
+        myFmt = mdates.DateFormatter('%d/%m')
         fig,ax = plt.subplots()
         if len(cols) <6:
             ax.plot(df1.b1_ch1,label = "Bearing 1")
@@ -93,18 +128,95 @@ def plot_mean_abs_general_stats(df,dataset_name):
         #save_fig(fig,"{}.png".format(dataset_name),figsize = (12,6),dpi = 300)
         fig.savefig(dataset_name+".pgf")
 
-pgf_imgs = Path("pgf_img")
-if not pgf_imgs.exists():
-    pgf_imgs.mkdir()
+def scatter_mean_abs_general_stats(df,dataset_name):
+    #cols = df.columns.to_list()[:-1]
+    df = df.abs()
+    df1 = df.groupby(["timestamp"]).mean().reset_index()
+    df1.index = pd.to_datetime(df1.timestamp,unit='s')
+    with plt.style.context('seaborn-paper'):
+        myFmt = mdates.DateFormatter('%d/%m')
+        fig,ax = plt.subplots()
+        ax.scatter(df1.b1_ch1,label = "Bearing 1 C-X")
+        ax.scatter(df1.b1_ch2,label = "Bearing 1 C-Y ")
+        ax.scatter(df1.b2_ch3,label = "Bearing 2 C-X")
+        ax.scatter(df1.b2_ch4,label = "Bearing 2 C-Y")
+        ax.scatter(df1.b3_ch5,label = "Bearing 3 C-X")
+        ax.scatter(df1.b3_ch6,label = "Bearing 3 C-Y")
+        ax.scatter(df1.b4_ch7,label = "Bearing 4 C-X")
+        ax.scatter(df1.b4_ch8,label = "Bearing 4 C-Y")
+        ax.set_xlabel ("Days")
+        ax.legend()
+        ax.xaxis.set_major_formatter(myFmt)
+        ax.grid()
+        ax.set_title("Scatter plot of absolute mean of the bearing vibration snapshots")
+        #save_fig(fig,"{}.png".format(dataset_name),figsize = (12,6),dpi = 300)
+        fig.savefig(dataset_name+".pgf")
 
-df1 = pd.read_pickle("../data/1st_test_full.pkl") # first dataset
-df2 = pd.read_pickle("../data/2nd_test_full.pkl") # second one
-df3 = pd.read_pickle("../data/3th_test_full.pkl") # third one
+def load_data():
+    global df1,df2,df3
+    df1 = pd.read_pickle("../data/1st_test_full.pkl") # first dataset
+    df2 = pd.read_pickle("../data/2nd_test_full.pkl") # second one
+    df3 = pd.read_pickle("../data/3th_test_full.pkl") # third one
 
-plot_rms_general_stats(df1,str(pgf_imgs/"rms_dataset1"))
-plot_mean_abs_general_stats(df1,str(pgf_imgs/"mean_abs_dataset1"))
-plot_rms_general_stats(df2,str(pgf_imgs/"rms_dataset2"))
-plot_mean_abs_general_stats(df2,str(pgf_imgs/"mean_abs_dataset2"))
-plot_rms_general_stats(df3,str(pgf_imgs/"rms_dataset3"))
-plot_mean_abs_general_stats(df3,str(pgf_imgs/"mean_abs_dataset3"))
+def df1_rep():
+    for index,i in enumerate([df1]):
+        index = index +1 
+        plot_rms_general_stats(i,str(pgf_imgs/f"rms_dataset{index}"))
+        plot_mean_abs_general_stats(i,str(pgf_imgs/f"mean_abs_dataset{index}"))
+        scatter_mean_abs_general_stats(i,str(pgf_imgs/f"mean_abs_dataset{index}"))
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_10_dataset{index}"),10)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_20_dataset{index}"),20)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_50_dataset{index}"),50)
+        
 
+
+def df2_rep():
+    for index,i in enumerate([df2]):
+        index = index +2
+        plot_rms_general_stats(i,str(pgf_imgs/f"rms_dataset{index}"))
+        plot_mean_abs_general_stats(i,str(pgf_imgs/f"mean_abs_dataset{index}"))
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_10_dataset{index}"),10)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_20_dataset{index}"),20)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_50_dataset{index}"),50)
+
+
+def df3_rep():
+    for index,i in enumerate([df3]):
+        index = index +3
+        plot_rms_general_stats(i,str(pgf_imgs/f"rms_dataset{index}"))
+        plot_mean_abs_general_stats(i,str(pgf_imgs/f"mean_abs_dataset{index}"))
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_10_dataset{index}"),10)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_20_dataset{index}"),20)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_50_dataset{index}"),50)
+
+
+def full_pipe():
+    for index,i in enumerate([df1,df2,df3]):
+        index = index +1 
+        plot_rms_general_stats(i,str(pgf_imgs/f"rms_dataset{index}"))
+        plot_mean_abs_general_stats(i,str(pgf_imgs/f"mean_abs_dataset{index}"))
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_10_dataset{index}"),10)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_20_dataset{index}"),20)
+        plot_rolling_average_stats(i,str(pgf_imgs/f"rolling_ave_period_50_dataset{index}"),50)
+
+
+if len(sys.argv) <= 1:
+    print("OPTIONS:\n full_pipe \n df1 \n df2 \n df3 ")
+elif sys.argv[1] == "full_pipe":
+    load_data()
+    full_pipe()
+elif sys.argv[1] == "df1":
+    load_data()
+    df1_rep()
+elif sys.argv[1] == "df2":
+    load_data()
+    df2_rep()
+elif sys.argv[1] == "df3":
+    load_data()
+    df3_rep()
+elif sys.argv[1] == "test":
+    print("lol")
+else:
+    print("\n Full pipeline")
+    load_data()
+    full_pipe()
