bd1baafd61e979220b4b94af50bbe538393c7740
jaimebw <jaimebwv@gmail.com>
2022-01-01 19:57:07 +0100

    Finished main part, need to add plots

--- scripts/classification_pipeline.py
+++ scripts/classification_pipeline.py
@@ -1,26 +1,27 @@
 """
 TO-DO:
-    1. Get all features data sets
-    2. Add labels to the data sets
-    3. Obtain results of the training ( of all three algos)
-    4. Obtain the plots and confussion matrix
+    1. Get all features data sets [X]
+    2. Add labels to the data sets[X]
+    3. Obtain results of the training ( of all three algos)[X]
+    4. Obtain the plots and confussion matrix[]
 """
 from pathlib import Path
 import pandas as pd
 import numpy as np
 import re
-from sklearn.metrics import confusion_matrix,
+from sklearn.metrics import confusion_matrix, precision_score,f1_score,recall_score,accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-kneigh = KNeighborsClassifier()
-dtree = DecisionTreeClassifier()
-forest  = RandomForestClassifier()
-algos = [kneigh,dtree,forest]
 RANDOM_STATE = 42
+kneigh = KNeighborsClassifier()
+dtree = DecisionTreeClassifier(random_state=RANDOM_STATE)
+forest  = RandomForestClassifier(random_state=RANDOM_STATE)
+algos = {"kneigh":kneigh,"dtree":dtree,"forest":forest}
+
 scaler = MinMaxScaler()
 input_labels = Path("../data/labels")
 input_path = Path("../data/feature_data")
@@ -50,25 +51,52 @@ for data in features_paths:
 data1.sort()
 data2.sort()
 data3.sort()
-labels = [pd.read_pickle(i) for i in input_labels.glob("*.pkl")]
+labels_paths = [i for i in input_labels.glob("*.pkl")]
+labels_paths.sort()
+labels = [pd.read_pickle(i) for i in labels_paths]
 # 2. Add labels to the dataset and train
+outer_index = 0
 for index, dir_paths in enumerate([data1,data2,data3]):
-    #index +=1
+    
     training_results = pd.DataFrame()
     for data_paths in dir_paths:
         data_name = data_paths.stem
+        print(f"Data set: {data_name}\n")
         df = pd.read_pickle(data_paths)
         df = df.join(labels[index])
-        df.dropna(inplace = True) # delete nans in values
+        df.dropna(inplace = True) 
         X = df[["b1_ch1","b2_ch2","b3_ch3","b4_ch4"]].values 
+        print(len(X))
         y = df[["labels"]].values
         y = np.ravel(y)
         X = scaler.fit_transform(X)
         X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=RANDOM_STATE)
         # 3. Train algos
-        for algo in algos:
+        for name,algo in algos.items():
             algo.fit(X_train,y_train)
-            # add scoring methods
+            y_pred = algo.predict(X_test)
+            confusion_matrix, precision_score,f1_score,recall_score,accuracy_score 
+            accuracy = accuracy_score(y_test,y_pred)
+            precission = precision_score(y_test,y_pred)
+            f1 = f1_score(y_test,y_pred)
+            recall = recall_score(y_test,y_pred) 
+            print(f"Algo: {name}\tDataset:{data_name}\tAccuracy:{accuracy}\tPrecission{precission}\tRecall:{recall}\tf1:{f1}\n")
+            algo_df = pd.DataFrame({
+                "algo":name,
+                "dataset":data_name,
+                "accuracy":accuracy,
+                "precission":precission,
+                "recall":recall,
+                "f1":f1
+
+            },index = [outer_index])
+            #algo_df["Algo"] = name
+            #algo_df["dataset"] = data_name
+            #algo_df["accuracy"] = accuracy
+            training_results = training_results.append(algo_df)
+            outer_index +=1 
+            print("-----------------------------------------------------\n")
+        training_results.to_csv(str(output_path)+f"/classification_results_set{index+1}.csv")
 
 
 
